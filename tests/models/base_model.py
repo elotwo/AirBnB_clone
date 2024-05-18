bf9ba990dbd1a15ab770281733fc999ce1062859
elotwo <obiagwu94@gmail.com>
2024-05-18 19:50:54 +0100

    me

--- tests/models/base_model.py
+++ tests/models/base_model.py
@@ -3,49 +3,34 @@ import uuid
 from datetime import datetime
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.my_number = 0
-        self.name = ''
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
                 if key != "__class__":
                     setattr(self, key, value)
-    def set_id(self, id=None):
-        if id is None:
+                if 'id' not in kwargs:
+                    self.id = str(uuid.uuid4())
+                if 'created_at' not in kwargs:
+                    created_at = datetime.now()
+                if 'updated_at'not in kwargs:
+                    updated_at = datetime.now()
+        else:
             self.id = str(uuid.uuid4())
-        else:
-            self.id = id
-    def set_name(self, name):
-        self.name = name
-    def set_my_number(self, my_number):
-        self.my_number = my_number
-    def set_created_at(self, created_at=None):
-        if created_at is None:
+            self.my_number = 0
+            self.name = ''
             self.created_at = datetime.now()
-        else:
-            self.created_at = created_at
-    def get_id(self):
-        return self.id
-    def get_name(self):
-        return self.name
-    def get_my_number(self):
-        return self.my_number
-    def get_created_at(self):
-        return self.created_at
-    def get_updated_at(self):
-        return self.updated_at
-    def set_updated_at(self):
-        self.updated_at = datetime.now()
+            self.updated_at = datetime.now()
     def save(self):
-        self.updated = datetime.now()
+        self.updated_at = datetime.now()
     def to_dict(self):
         dict_repr = self.__dict__.copy()
         dict_repr["__class__"] = self.__class__.__name__
         dict_repr["created_at"] = self.created_at.isoformat()
         dict_repr["updated_at"] = self.updated_at.isoformat()
         return dict_repr
+    def to_json_string(self):
+        return json.dumps(self.to_dict(), indent=4)
     def __str__(self):
         return f"[BaseModel]({self.id}, 'name': {self.name}, 'my_number': {self.my_number}, 'updated_at': {self.updated_at}, 'created_at': {self.created_at})"
     def __repr__(self):
