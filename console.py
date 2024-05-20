5c5b1d27140f4c76628ba45e589a4a4631548da3
elotwo <obiagwu94@gmail.com>
2024-05-20 13:56:41 +0100

    me

--- console.py
+++ console.py
@@ -5,17 +5,18 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
 
-    def do_quit(self, line):
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
 
-        """greet [person]
-        Greet the named person """
-        if person:
-
-            print("hi,", person)
-        else:
-            print("hi")
-    def do_EOF(self, line):
-        return quit
+    def do_help(self, arg):
+        """Help cammand list about a command"""
+        super().do_help(arg)
+    def emptyline(self):
+        pass 
+    def do_EOF(self, arg):
+        """this signal the end of File"""
+        return True
 
 
 if __name__ == "__main__":
