2f180a601adbe04bf87368b9e554b8487a7a0dcc
elotwo <obiagwu94@gmail.com>
2024-05-22 00:41:02 +0100

    me

--- tests/models/base_model.py
+++ tests/models/base_model.py
@@ -1,40 +1,45 @@
 #!/usr/bin/python3
+"""
+the class Basemodel set a uniq id, name, created time and as well the time it was updated
+"""
 import uuid
 from datetime import datetime
 import json
+
 class BaseModel:
+    """
+    class Basemodel
+    """
     def __init__(self, *args, **kwargs):
         if kwargs:
+            self.id = str(uuid.uuid4())
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.fromisoformat(value)
                 if key != "__class__":
-                    setattr(self, key, value)
-                if 'id' not in kwargs:
-                    self.id = uuid.uuid4()
-                if 'created_at' not in kwargs:
-                    created_at = datetime.now()
-                if 'updated_at'not in kwargs:
-                    updated_at = datetime.now()
+                    if key in ["created_at", "updated_at"]:
+                        value = datetime.strptime(value,"%Y-%m-%dT%H:%M:%S.%f")
+                        setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.my_number = 0
             self.name = ''
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+
+    def __str__(self):
+        """ the string function prints all BaseModel atrribute in strings """
+        return f"[{self.__class__.__name__}]({(self.id)}) {self.__dict__}"
     def save(self):
+        """ function save the updated time """
         self.updated_at = datetime.now()
-        self.created_at = datetime.now()
+
     def to_dict(self):
+        """
+        this function convert object to dictionary
+        """
         dict_repr = self.__dict__.copy()
         dict_repr["__class__"] = self.__class__.__name__
         dict_repr["created_at"] = self.created_at.isoformat()
         dict_repr["updated_at"] = self.updated_at.isoformat()
         return dict_repr
-    def to_json_string(self):
-                return json.dumps(self.to_dict(), indent=4)
-    def __str__(self):
-        return f"[BaseModel]({self.id}, 'name': {self.name}, 'my_number': {self.my_number}, 'updated_at': {self.updated_at}, 'created_at':datetime.datetime({self.created_at}))"
-    def __repr__(self):
-        return self.__str__()
-    i
+
+
